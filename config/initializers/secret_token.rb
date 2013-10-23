42ca4ccbec2e89dcb0edff1f3919ca03b4859036
cimbriano <Christopher.Imbriano@gmail.com>
2013-10-23 11:05:01 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TrendSourceFinder::Application.config.secret_key_base = '30f54244acf24d21f9112c6400c876becc98770272957e74389b1bc55899afd11219a86b90f208077552afc410743c833b9ec5601458e7603e83f91529a18def'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TrendSourceFinder::Application.config.session_store :cookie_store, key: '_trend-source-finder_session'

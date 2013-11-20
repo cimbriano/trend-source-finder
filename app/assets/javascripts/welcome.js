d4d9e6c48e1ee0e3302d0bc5b3ac57f991e7d308
jinsungit <jinanderson@gmail.com>
2013-11-20 18:47:50 -0500

    Graph now shos network of tweets, black node means tweet that not in our database

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -25,16 +25,19 @@ function visualizeit(){
 
 
     var initialScaleData = [];
+    var twitterID = [];
+    var isNodeInGraph = {};
 
     for (var i=0;i<tweets.length;i++)
     {
       initialScaleData[i] = parseInt(tweets[i].created_at_numeric);
+      twitterID[i] = tweets[i].twitter_id;
+      isNodeInGraph[twitterID[i]] = 0;
     }
 
     var linearScale = d3.scale.linear()
                     .domain([d3.min(initialScaleData),d3.max(initialScaleData)])
                     .range([0,width])
-
 
     var circleAttributes = circles
                          .attr("cx", function(d){return linearScale(parseInt(d.created_at_numeric))})
@@ -54,11 +57,30 @@ function visualizeit(){
 
 
 var G = jsnx.DiGraph();
+
+for(var i=0;i<tweets.length;i++)
+{
+  if (isNodeInGraph[twitterID[i]] == 0)//node is not in graph yet
+  {
+    G.add_nodes_from([twitterID[i]],{color:'#0064C7'});
+    isNodeInGraph[twitterID[i]] = 1;
+  }
+  if (!jQuery.isEmptyObject(tweets[i].retweeted_id))
+  {
+    parentID = tweets[i].retweeted_id;
+    if (isNodeInGraph[parentID]==0)
+    {
+      G.add_nodes_from([parentID],{color:'#0064C7'});
+      isNodeInGraph[parentID] = 1;
+    }
+    G.add_edge(twitterID[i],parentID);
+  }
+// G.add_nodes_from([1,2,3,4,5,[9,{color: '#008A00'}]], {color: '#0064C7'});
+// G.add_cycle([1,2,3,4,5]);
+// G.add_edges_from([[1,9], [9,1]]);
  
-G.add_nodes_from([1,2,3,4,5,[9,{color: '#008A00'}]], {color: '#0064C7'});
-G.add_cycle([1,2,3,4,5]);
-G.add_edges_from([[1,9], [9,1]]);
- 
+}
+
  jsnx.draw(G, {
     element: '#canvas', 
     with_labels: true, 
@@ -67,7 +89,7 @@ G.add_edges_from([[1,9], [9,1]]);
             return d.data.color; 
         }
     }, 
-    label_style: {fill: 'white' }
+    label_style: {fill: 'red' }
 });
 
 }

d6e852fade59ca07fefd5f49a76df31cddb6e694
jinsungit <jinanderson@gmail.com>
2013-11-20 19:02:49 -0500

    switch to edges

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,83 +1,40 @@
-var tweets;
+var edges;
 
-d3.json("/tweets.json", function(error, json){
+d3.json("/edges.json", function(error, json){
 	if(error) return console.warn(error);
-	tweets = json;
+	edges = json;
 	visualizeit();
 });
 
 function visualizeit(){
-    var width = 600;
-    var svgContainer = d3.select(".container").append("svg")
-                                       .attr("width", width)
-                                       .attr("height", 200);
-
-    var circles = svgContainer.selectAll("circle")
-                             .data(tweets)
-                             .enter()
-                            .append("circle");
-
-    // text element
-    var text = svgContainer.selectAll("text")
-                          .data(tweets)
-                          .enter()
-                          .append("text");
 
 
-    var initialScaleData = [];
-    var twitterID = [];
     var isNodeInGraph = {};
 
-    for (var i=0;i<tweets.length;i++)
+    for (var i=0;i<edges.length;i++)
     {
-      initialScaleData[i] = parseInt(tweets[i].created_at_numeric);
-      twitterID[i] = tweets[i].twitter_id;
-      isNodeInGraph[twitterID[i]] = 0;
+      isNodeInGraph[edges[i].child_id] = 0;
+      isNodeInGraph[edges[i].parent_id] = 0;
     }
-
-    var linearScale = d3.scale.linear()
-                    .domain([d3.min(initialScaleData),d3.max(initialScaleData)])
-                    .range([0,width])
-
-    var circleAttributes = circles
-                         .attr("cx", function(d){return linearScale(parseInt(d.created_at_numeric))})
-                         .attr("cy", function(d){return 100})
-                         .attr("r", function (d) { return d.id+10; })
-                         .style("fill", function(d) {
-                         	return "purple";
-                         });
-
-    var textLabels = text
-                    .attr("x", function(d){return linearScale(parseInt(d.created_at_numeric))})
-                    .attr("y", function(d){return d.id*10})
-                    .text(function(d){return "CreatedAt: "+d.created_at+"\n scaled: "+linearScale(parseInt(d.created_at_numeric));})
-                    .attr("font-family", "sans-serif")
-                    .attr("font-size", "10px")
-                    .attr("fill","red");
 
 
 var G = jsnx.DiGraph();
 
-for(var i=0;i<tweets.length;i++)
+for(var i=0;i<edges.length;i++)
 {
-  if (isNodeInGraph[twitterID[i]] == 0)//node is not in graph yet
+  child_id = edges[i].child_id;
+  parent_id = edges[i].parent_id;
+  if (isNodeInGraph[child_id] == 0)//node is not in graph yet
   {
-    G.add_nodes_from([twitterID[i]],{color:'#0064C7'});
-    isNodeInGraph[twitterID[i]] = 1;
+    G.add_nodes_from([child_id],{color:'#0064C7'});
+    isNodeInGraph[child_id] = 1;
   }
-  if (!jQuery.isEmptyObject(tweets[i].retweeted_id))
+  if (isNodeInGraph[parent_id] == 0)//node is not in graph yet
   {
-    parentID = tweets[i].retweeted_id;
-    if (isNodeInGraph[parentID]==0)
-    {
-      G.add_nodes_from([parentID],{color:'#0064C7'});
-      isNodeInGraph[parentID] = 1;
-    }
-    G.add_edge(twitterID[i],parentID);
+    G.add_nodes_from([parent_id],{color:'#0064C7'});
+    isNodeInGraph[parent_id] = 1;
   }
-// G.add_nodes_from([1,2,3,4,5,[9,{color: '#008A00'}]], {color: '#0064C7'});
-// G.add_cycle([1,2,3,4,5]);
-// G.add_edges_from([[1,9], [9,1]]);
+  G.add_edge(parent_id,child_id);
  
 }
 

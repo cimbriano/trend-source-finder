f8d79c0078d5006acb766d4a9070692c42af484b
cimbriano <Christopher.Imbriano@gmail.com>
2013-11-23 16:27:29 -0500

    Draws links, but not correctly

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,10 +1,10 @@
 var data;
 
-d3.json("/tweets.json", function(error, json){
-        if(error) return console.warn(error);
-        data = json;
-        visualizeit();
-        });
+d3.json("/graph.json", function(error, json){
+  if(error) return console.warn(error);
+  data = json;
+  visualizeit();
+});
 
 function visualizeit(){
     var width = 960;
@@ -21,39 +21,44 @@ function visualizeit(){
     .attr("width", width)
     .attr("height", height);
     
-    // var link = svg.selectAll(".link");
+    var link = svg.selectAll(".link");
     var node = svg.selectAll(".node");
     
     // Make a linear scale for the x-postion
     var initialScaleData = [];
-    for(var i = 0; i < data.length; i++) {
-        initialScaleData[i] = data[i].created_at_numeric;
+
+    for(var i = 0; i < data.tweets.length; i++) {
+        initialScaleData[i] = data.tweets[i].created_at_numeric;
     }
-    
+
     var linearScale = d3.scale.linear()
-    .domain([d3.min(initialScaleData), d3.max(initialScaleData)])
-    .range([padding, width - padding])
+      .domain([d3.min(initialScaleData), d3.max(initialScaleData)])
+      .range([padding, width - padding])
     
     var xAxis = d3.svg.axis()
-    .scale(linearScale)
-    .orient("bottom")
-    .ticks(10)
-    .tickFormat(function(d) { var date = new Date(d*1000); return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDay(); });
+      .scale(linearScale)
+      .orient("bottom")
+      .ticks(10)
+      .tickFormat(function(d) { var date = new Date(d*1000); return date.getFullYear() + "-" + date.getMonth() + "-" + date.getDay(); });  
     
     svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(0," + (height - padding) + ")")
-    .call(xAxis);
+      .attr("class", "axis")
+      .attr("transform", "translate(0," + (height - padding) + ")")
+      .call(xAxis);
     
     
+    // console.log("data.tweets.length: " + data.tweets.length);
+
     force
-    .nodes(data)
-    // .links(graph.links)
-    .start();
+      .nodes(data.tweets)
+      .links(data.edges)
+      .start();
 
-  // link = 
+  link = link.data(data.edges)
+             .enter().append("line")
+             .attr("class", "link");
 
-  node = node.data(data)
+  node = node.data(data.tweets)
              .enter().append("circle")
                 .attr("class", "node")
                 .attr("cx", function(d) { x = linearScale(d.created_at_numeric); console.log(x); return x; })
@@ -62,10 +67,10 @@ function visualizeit(){
                 .on("click", nodeClick);
 
   function tick() {
-    // link.attr("x1", function(d) { return d.source.x; })
-    //     .attr("y1", function(d) { return d.source.y; })
-    //     .attr("x2", function(d) { return d.target.x; })
-    //     .attr("y2", function(d) { return d.target.y; });
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
 
     node.attr("cy", function(d) { return d.y; });
     // .attr("cx", function(d) { return d.x; })
